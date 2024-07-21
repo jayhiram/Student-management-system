d4f688684e8769fdbbbea5a4115bc44e5bb46da5
Wangui Hiram <116981763+jayhiram@users.noreply.github.com>
2024-07-21 17:34:39 +0300

    Update admin.py

--- students/admin.py
+++ students/admin.py
@@ -6,9 +6,4 @@ admin.site.register(Student)
 admin.site.register(Course)
 admin.site.register(Attendance)
 
-from django.contrib import admin
-from .models import Student, Course, Attendance
 
-admin.site.register(Student)
-admin.site.register(Course)
-admin.site.register(Attendance)
